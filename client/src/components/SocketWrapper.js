3dfd7c3d3dbe93b11cadb2b396719ab4f8f98cf0
Yash-007 <102963640+Yash-007@users.noreply.github.com>
2023-10-11 17:51:25 +0530

    Update SocketWrapper.js

--- client/src/components/SocketWrapper.js
+++ client/src/components/SocketWrapper.js
@@ -20,7 +20,7 @@ function addPropsToChildren(children, props) {
 }
 
 export default function SocketWrapper({ children }) {
-    const socket = io.connect(process.env.REACT_APP_WEB_SOCKET_URL || "http://localhost:5000")
+    const socket = io.connect(process.env.REACT_APP_WEB_SOCKET_URL || "https://guileless-praline-e4273a.netlify.app/")
 
     const location = useLocation()
     const navigate = useNavigate()
